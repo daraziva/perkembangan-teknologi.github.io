f1a8d46a4aac73f4af090f9b4532ee1c73424641
daraziva <darazivatabina@smkwikrama.sch.id>
2023-06-10 11:06:14 +0700

    perkembangan revolusi industri

---
+++ resources/views/home.blade.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Sejarah Perkembangan</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Amatic+SC:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Yummy
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>daraziva<span>.</span></h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#menu">Example</a></li>
+          
+      </nav><!-- .navbar -->
+
+      
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center section-bg">
+    <div class="container">
+      <div class="row justify-content-between gy-5">
+        <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
+          <h2 data-aos="fade-up">Sejarah perkembangan teknologi manusia mulai <br>dari revolusi industri-sekarang.</h2>
+          <p data-aos="fade-up" data-aos-delay="100">Dara Ziva Tabina|PPLG XI-5|Cicurug2</p>
+          <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+
+            <a href="https://youtu.be/Mee7At7Kvtw" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+          </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2 text-center text-lg-start">
+          <img src="assets/img/wikrama-logo.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="300">
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero Section -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <p>Sejarah Perkembangan <span>revolusi</span></p>
+        </div>
+        <p style="text-align:justify;"><img src="assets/img/first.webp" style="float:left; margin:100 9px 30px 110;" class="col-lg-7 position-relative about-img"/>
+        Sejarah revolusi industri pertama kali terjadi di negara Inggris pada tahun 1760 (Abad ke-18) yang mana saat itu kondisi negaranya berada dalam keadaan stabil.
+
+Revolusi industri 1.0 membawa perubahan yang cukup besar, di mana pekerjaan-pekerjaan yang dahulunya dikerjakan secara kasar oleh tangan manusia, perlahan digantikan oleh mesin-mesin bertenaga uap.
+
+Era revolusi industri 1.0 pun berakhir, ditandai dengan ditemukannya tenaga listrik sekitar tahun 1870-an. Era ini dinamakan revolusi industri 2.0. Nama-nama besar seperti Thomas Alva Edison, Nikola Tesla, dan sebagainya hadir mewarnai era ini.
+
+Pada era inilah dunia industri manufaktur mampu merakit benda-benda seperti lampu modern, mobil, pesawat, telegraf, dan lain-lain.
+
+Setelah revolusi industri 2.0 berakhir, muncul era baru yang dinamakan era revolusi industri 3.0. Pada era ini, dunia industri semakin lebih efisien dengan adanya otomatisasi di berbagai hal. Era ini melahirkan teknologi seperti komputer, internet, perangkat lunak, dan lain-lain.
+
+Kemudian pada abad 21, kita semua telah berada di era 4.0. Beberapa sumber menyebutkan revolusi industri 4.0 dimulai pada tahun 2011, saat proyek-proyek berteknologi tinggi mulai digarap oleh beberapa pihak.
+
+Era revolusi industri 4.0 yang saat ini ini kita jalani telah melahirkan berbagai macam teknologi mutakhir, dan akan terus berkembang dalam beberapa tahun mendatang.</p>
+            </div>
+          </div>
+            </div>
+          </div>
+        </div>
+      
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row gy-4">
+
+
+              <div class="col-xl-4" data-aos="fade-up" data-aos-delay="300">
+                <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+                  <i class="bi bi-gem"></i>
+                  <h4>Revolusi Industri 1.0</h4>
+                  <p>Revolusi Industri 1.0 adalah era yang terjadi pada abad ke-18 (1760–1840). Pada kisaran tahun tersebut, penemuan mesin uap oleh James Watt</p>
+                </div>
+              </div><!-- End Icon Box -->
+
+              <div class="col-xl-4" data-aos="fade-up" data-aos-delay="400">
+                <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+                  <i class="bi bi-inboxes"></i>
+                  <h4>Perkembangan Revolusi Industri 2.0</h4>
+                  <p>Revolusi Industri 2.0 adalah era revolusi yang terjadi sekitar awal abad ke-19 (1870-an) dan berfokus kepada efisiensi mesin di setiap lini (Assembly Line) dalam proses produksi karena ditemukannya tenaga listrik.</p>
+                </div>
+              </div><!-- End Icon Box -->
+
+              <div class="col-xl-4" data-aos="fade-up" data-aos-delay="300">
+                <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+                  <i class="bi bi-gem"></i>
+                  <h4>Perkembangan Revolusi Industri 3.0</h4>
+                  <p>Revolusi Industri 3.0 adalah era yang terjadi sekitar awal abad ke-20 (1970-an) dan dipicu oleh perkembangan mesin-mesin pintar (Komputer & Software) berbasis teknologi otomatisasi yang perlahan menggantikan peran-peran manusia di lapangan. </p>
+                </div>
+              </div><!-- End Icon Box -->
+
+              <div class="col-xl-4" data-aos="fade-up" data-aos-delay="400">
+                <div class="icon-box d-flex flex-column justify-content-center align-items-center">
+                <div class="child"></div>
+                  <i class="bi bi-inboxes"></i>
+                  <h4>Perkembangan Revolusi Industri 4.0</h4>
+                  <p>Revolusi Industri 4.0 adalah era yang saat ini kita jalani di mana pengembangan teknologi lebih lanjut seperti internet berkecepatan tinggi, merevolusi setiap proses mulai dari produksi hingga distribusi dan berfokus kepada keberlanjutan (Sustainability).</p>
+                </div>
+              </div><!-- End Icon Box -->
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+
+    <!-- ======= Menu Section ======= -->
+    <section id="menu" class="menu">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>Contoh</h2>
+          <p>Teknologi <span>Revolusi Industri</span></p>
+        </div>
+
+        <ul class="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
+
+          <li class="nav-item">
+            <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#menu-starters">
+              <h4>Industri 1.0</h4>
+            </a>
+          </li><!-- End tab nav item -->
+
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-breakfast">
+              <h4>Industri 2.0</h4>
+            </a><!-- End tab nav item -->
+
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-lunch">
+              <h4>Industri 3.0</h4>
+            </a>
+          </li><!-- End tab nav item -->
+
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#menu-dinner">
+              <h4>Industri 4.0</h4>
+            </a>
+          </li><!-- End tab nav item -->
+
+        </ul>
+
+        <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
+
+          <div class="tab-pane fade active show" id="menu-starters">
+
+            <div class="tab-header text-center">
+              <h3>Perkembangan Revolusi Industri 1.0</h3>
+            </div>
+
+            <div class="row gy-5">
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/1.1.png" class="glightbox"><img src="assets/img/1.1.png" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+                <p class="ingredients">
+      
+                </p> 
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/1.2.jpeg" class="glightbox"><img src="assets/img/1.2.jpeg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+      
+              </div><!-- Menu Item -->
+
+            </div>
+          </div><!-- End Starter Menu Content -->
+
+          <div class="tab-pane fade" id="menu-breakfast">
+
+            <div class="tab-header text-center">
+              <h3>Perkembangan Revolusi Industri 2.0</h3>
+            </div>
+
+            <div class="row gy-5">
+
+              
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/2.2.jpg" class="glightbox"><img src="assets/img/2.2.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/2.3.jpg" class="glightbox"><img src="assets/img/2.3.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/2.4.jpg" class="glightbox"><img src="assets/img/2.4.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+            </div>
+          </div><!-- End Breakfast Menu Content -->
+
+          <div class="tab-pane fade" id="menu-lunch">
+
+            <div class="tab-header text-center">
+              <h3>Perkembangan Revolusi Industri 3.0</h3>
+            </div>
+
+            <div class="row gy-5">
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/3.1.jpg" class="glightbox"><img src="assets/img/3.1.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/3.2.webp" class="glightbox"><img src="assets/img/3.2.webp" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/3.3.jpg" class="glightbox"><img src="assets/img/3.3.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+             
+            </div>
+          </div><!-- End Lunch Menu Content -->
+
+          <div class="tab-pane fade" id="menu-dinner">
+
+            <div class="tab-header text-center">
+              <h3>Perkembangan Revolusi Industri 4.0</h3>
+            </div>
+
+            <div class="row gy-5">
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/4.1.jpg" class="glightbox"><img src="assets/img/4.1.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/4.2.jpg" class="glightbox"><img src="assets/img/4.2.jpg" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="assets/img/4.3.webp" class="glightbox"><img src="assets/img/4.3.webp" class="menu-img img-fluid" alt=""></a>
+                <h4></h4>
+              </div><!-- Menu Item -->
+
+            </div>
+          </div><!-- End Dinner Menu Content -->
+
+        </div>
+
+      </div>
+    </section><!-- End Menu Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2></h2>
+          <p>Terimakasih <span>Untuk Perhatian nya!</span></p>
+        </div>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+   
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
